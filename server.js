2fd790b82dab941f3a4fef4b3a71898f8771e613
Deivid Molina <davidglezm2015@gmail.com>
2024-07-16 21:04:50 -0600

    Cambios generales en server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const salt = 10;
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://app-techcomp-16ff4d30c364.herokuapp.com"],
+    origin: ["https://app-techcomp-16ff4d30c364.herokuapp.com"],
     methods: ["POST", "GET"],
     credentials: true
 }));
