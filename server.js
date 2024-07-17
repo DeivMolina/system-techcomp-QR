41a0483fd53e4a1ea3edd022a4c902ae8e763e0f
Deivid Molina <davidglezm2015@gmail.com>
2024-07-16 21:52:36 -0600

    Correccion env

--- server.js
+++ server.js
@@ -47,7 +47,7 @@ const db = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
+    database: process.env.DB_DATABASE
 });
 
 // Verificación de usuario-token-cookies
