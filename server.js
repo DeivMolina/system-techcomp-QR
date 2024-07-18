c12a8c40e930333c2914f8ab92bf2a2796665e82
Deivid Molina <davidglezm2015@gmail.com>
2024-07-18 15:01:01 -0600

    Arreglo de rutas de archivos v3

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const salt = 10;
 const app = express();
 app.use(express.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://app-techcomp-16ff4d30c364.herokuapp.com", "https://front-techcomp.rkcreativo.com.mx"],
+    origin: ["http://localhost:3000", "https://app-techcomp-16ff4d30c364.herokuapp.com", "https://front-techcomp.rkcreativo.com.mx", "https://front-techcomp.rkcreativo.com.mx/upload"],
     methods: ["POST", "GET"],
     credentials: true
 }));
